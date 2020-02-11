1e914f81baa11c6828cad1c41b7b57e2285359ba
ChiAkuma <chiakuma13@gmail.com>
2020-02-11 20:03:43 +0100

    Fenster in der mitte des Bildschirms in GameController.java

--- src/de/polygondev/hjp/ctrl/GameController.java
+++ src/de/polygondev/hjp/ctrl/GameController.java
@@ -3,6 +3,8 @@ package de.polygondev.hjp.ctrl;
 import de.cg.cgge.game.GameInstance;
 import de.cg.cgge.game.Room;
 
+import java.awt.*;
+
 public class GameController {
 
     private GameInstance game;
@@ -10,7 +12,14 @@ public class GameController {
     public GameController() {
 
         game = new GameInstance();
-
+        
+        Dimension toolkit = Toolkit.getDefaultToolkit().getScreenSize();
+        game.getDrawer().getWindow().setBounds(
+                (int) toolkit.getWidth()/2 -game.getWidth()/2,
+                (int) toolkit.getHeight()/2 -game.getHeight()/2,
+                (int) game.getWidth(),
+                (int) game.getHeight());
+        
         Room testRoom = new RoomBuilder(game).initFromFile(0).build();
         game.getDrawer().changeRoomSafely(testRoom);
     }
