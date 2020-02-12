793158a65a5e976330a11c7a2fb5fd77617a7a03
CrazyGamesMC <CrazyGamesMC@users.noreply.github.com>
2020-02-12 17:29:02 +0100

    Player sprites

---
+++ src/de/polygondev/hjp/animations/AnimationController.java
@@ -0,0 +1,17 @@
+package de.polygondev.hjp.animations;
+
+import de.cg.cgge.game.CameraRenderer;
+
+import java.awt.*;
+
+public class AnimationController {
+
+    public AnimationController() {
+
+    }
+
+    public void draw(int x, int y, CameraRenderer cr) {
+
+    }
+
+}
