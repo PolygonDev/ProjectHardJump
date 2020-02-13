aeb167d41a0010565994839f53c94fc2aaa6fa28
CrazyGamesMC <CrazyGamesMC@users.noreply.github.com>
2020-02-13 16:50:43 +0100

    Started adding spikes

---
+++ src/de/polygondev/hjp/objects/ingame/Spikes.java
@@ -0,0 +1,10 @@
+package de.polygondev.hjp.objects.ingame;
+
+import de.cg.cgge.game.GameObject;
+import de.cg.cgge.game.Room;
+
+public class Spikes extends GameObject {
+    public Spikes(Room room) {
+        super(room);
+    }
+}
