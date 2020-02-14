2d76845c71be99131e9106394ea8802ff1a5f2ef
ChiAkuma <chiakuma13@gmail.com>
2020-02-14 14:41:34 +0100

    versucht ground größe an textur anzupassen

--- src/de/polygondev/hjp/objects/ui/GameUI.java
+++ src/de/polygondev/hjp/objects/ui/GameUI.java
@@ -14,8 +14,19 @@ public class GameUI extends GameObject {
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Resources.color_game_background);
-        g.fillRect(0,0,room.getGameInstance().getWidth(),room.getGameInstance().getHeight());
+        //g.setColor(Resources.color_game_background);
+    
+        Graphics2D g2d = (Graphics2D) g;
+        
+        Color startColor = Resources.color_game_background;
+        Color endColor = Color.blue;
+    
+        int startX = room.getGameInstance().getWidth(), startY = room.getGameInstance().getHeight(), endX = room.getGameInstance().getWidth(), endY = 0;
+    
+        GradientPaint gradient = new GradientPaint(startX, startY, startColor, endX, endY, endColor);
+        g2d.setPaint(gradient);
+        
+        g2d.fillRect(0,0,room.getGameInstance().getWidth(),room.getGameInstance().getHeight());
     }
 
 }
